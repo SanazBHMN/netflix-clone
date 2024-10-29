1a7a42cf8ae7aa6b75fe37e2d1a9fea7f71a0bd6
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-10-29 11:31:25 +0330

    Create a movie list GET endpoint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const movies = require("./movies.json");
 
 const app = express();
 
 app.get("/", (req, res) => {
   return res.send("HELLO THERE");
+});
+
+app.get("/movies/list", (req, res) => {
+  return res.send(movies);
 });
 
 app.listen(8080, () => {
