d0d78b4ae4898aaf20d245e3c55f8373af029da3
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-10-29 11:02:29 +0330

    Define routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,10 @@
-// import pages
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import PlansPage from "./pages/PlansPage";
-import BrowsePage from "./pages/BrowsePage";
-import WatchPage from "./pages/WatchPage";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      {/* <HomePage /> */}
-      {/* <LoginPage /> */}
-      {/* <PlansPage /> */}
-      {/* <BrowsePage /> */}
-      <WatchPage />
+      <Outlet />
     </div>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+// pages
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import PlansPage from "./pages/PlansPage.tsx";
+import BrowsePage from "./pages/BrowsePage.tsx";
+import WatchPage from "./pages/WatchPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/plans" element={<PlansPage />} />
+      <Route path="/browse" element={<BrowsePage />} />
+      <Route path="/browse/watch/:id" element={<WatchPage />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
