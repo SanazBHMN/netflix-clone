dae7718638f365810526292de68cedca133a4cb8
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-11-05 14:45:16 +0330

    Detecting the visibility of an element

--- client/src/pages/BrowsePage.tsx
+++ client/src/pages/BrowsePage.tsx
@@ -2,11 +2,30 @@ import Navbar from "../components/Navbar";
 import Billboard from "../components/Billboard";
 import MovieList from "../components/MovieList";
 import useMoviesList from "../hooks/useMoviesList";
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 
 function BrowsePage() {
   const [offset, setOffset] = useState(12);
   const { data, loading, error } = useMoviesList(offset);
+
+  const observer = useRef<null | IntersectionObserver>(null);
+
+  const lastElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (loading) return;
+
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setOffset(offset + 12);
+          console.log("INTERSECTING");
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [loading]
+  );
 
   // console.log(data, loading, error);
 
@@ -17,7 +36,7 @@ function BrowsePage() {
       <div className="pb-5">
         {loading && <p>LOADING...</p>}
         {error && <p>{error}</p>}
-        {data && <MovieList movies={data} />}
+        {data && <MovieList movies={data} lastElementRef={lastElementRef} />}
       </div>
     </div>
   );
