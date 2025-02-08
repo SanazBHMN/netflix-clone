4373a2608ac896cbcc5fa9d826918ef219d913ac
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-02-08 10:24:40 +0330

    Validating user

--- server/index.js
+++ server/index.js
@@ -4,36 +4,16 @@ const cors = require("cors");
 
 const app = express();
 
-const { prisma } = require("./db");
-
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
   return res.send("HELLO THERE");
 });
 
-app.get("/movies/list", async (req, res) => {
-  const offset = parseInt(req.query.offset);
+app.use("/auth", require("./routes/auth"));
 
-  const count = await prisma.movie.count();
-
-  const movies = await prisma.movie.findMany({
-    skip: offset,
-    take: 12,
-  });
-
-  return res.json({ movies, count });
-});
-
-app.get("/movie/:id", async (req, res) => {
-  const id = req.params.id;
-  const movie = await prisma.movie.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
-  return res.send(movie);
-});
+app.use("", require("./routes/movies"));
 
 app.listen(8080, () => {
   console.log("LISTENING ON PORT 8080");
---
+++ server/routes/auth.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const { prisma } = require("../db");
+const { check, validationResult } = require("express-validator");
+
+router.post(
+  "/signup",
+  [
+    check("email", "Please input a valid email").isEmail(),
+    check(
+      "password",
+      "Please input a password with a min length of 6"
+    ).isLength({ min: 6 }),
+    check(
+      "username",
+      "Please input a username with a min length of 6"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { email, password, username } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return res.status(400).json({
+        errors: [{ msg: "This user already exists" }],
+      });
+    }
+
+    res.send("VALID");
+
+    console.log(errors);
+  }
+);
+
+module.exports = router;
