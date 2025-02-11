66b4f1060a2a3f1fb25fd0495ae6928969d0b59a
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-02-11 15:04:27 +0330

    Fix the sign up/in form

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,15 +1,22 @@
-function Input() {
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+}
+
+function Input({ id, label, type }: InputProps) {
   return (
     <div className="relative">
       <input
+        id={id}
+        type={type}
         className="block rounded-md px-6 pt-6 pb-1 w-full text-base"
-        type="text"
       />
       <label
         htmlFor=""
         className="absolute text-base text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
       >
-        Email
+        {label}
       </label>
     </div>
   );
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,25 +1,53 @@
+import { useState } from "react";
 import Input from "../components/Input";
 import Navbar from "../components/Navbar";
 
+enum Variant {
+  SIGN_UP,
+  LOGIN,
+}
+
 function LoginPage() {
+  const [variant, setVariant] = useState(Variant.LOGIN);
+
   return (
     <div className="relative bg-black h-screen w-screen bg-opacity-50">
       <Navbar />
       <div className="flex justify-center items-center h-full">
         <div className="bg-black bg-opacity-70 p-16 self-center mt-2 w-full max-w-md rounded-md">
-          <h2 className="text-white text-3xl mb-8 font-semibold">Sign in</h2>
+          <h2 className="text-white text-3xl mb-8 font-semibold">
+            {variant === Variant.SIGN_UP ? "Sign Up" : "Login"}
+          </h2>
           <form className="flex flex-col gap-4">
-            <Input />
+            {variant === Variant.SIGN_UP && (
+              <Input id="username" type="text" label="Username" />
+            )}
+            <Input id="email" type="email" label="Email address" />
+            <Input id="password" type="password" label="Password" />
             <input
               type="submit"
               className="bg-red-400 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
             />
           </form>
-          <p className="text-neutral-500 mt-12">
-            <span className="text-white ml-1 hover:underline cursor-pointer">
-              First time using Netflix?
-            </span>
-          </p>
+          {variant === Variant.LOGIN ? (
+            <p
+              className="text-neutral-500 mt-12"
+              onClick={() => setVariant(Variant.SIGN_UP)}
+            >
+              <span className="text-white ml-1 hover:underline cursor-pointer">
+                First time using Netflix?
+              </span>
+            </p>
+          ) : (
+            <p
+              className="text-neutral-500 mt-12"
+              onClick={() => setVariant(Variant.LOGIN)}
+            >
+              <span className="text-white ml-1 hover:underline cursor-pointer">
+                Already have an account?
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>
